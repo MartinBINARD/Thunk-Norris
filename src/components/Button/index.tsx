c6cdb46b430ae2daf1aeed47e41e39b23715d6e5
MartinBINARD <78610464+MartinBINARD@users.noreply.github.com>
2023-09-05 16:47:08 +0200

    Initial commit

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import './index.scss';
+
+function Button() {
+  return (
+    <button type="button" className="button">Nouvelle blague</button>
+  );
+}
+
+export default Button;
---
+++ src/components/Widget/index.tsx
@@ -0,0 +1,13 @@
+import './index.scss';
+import Button from '../Button';
+
+function Widget() {
+  return (
+    <article className="widget">
+      <p className="widget-content">Un super blague hilarante</p>
+      <Button />
+    </article>
+  );
+}
+
+export default Widget;
